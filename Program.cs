48fb7cb54ecb9dc17d4882c441b826a840013673
Diego51602 <136129720+Diego51602@users.noreply.github.com>
2026-06-20 18:29:40 -0600

    Migrate from SQL Server to PostgreSQL

--- Program.cs
+++ Program.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using TaskManagerAPI.Data;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Base de datos
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // JWT
 var jwtKey = builder.Configuration["Jwt:Key"]!;
